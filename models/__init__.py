b02101beb994011540079ee7810769afd37c9f6b
brianneoberson <brianne.oberson@epfl.ch>
2023-05-10 17:20:46 +0200

    Added init file.

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+'''
+The __init__.py files are required to make Python treat directories containing the file as packages. 
+In the simplest case, __init__.py can just be an empty file, but it can also execute initialization code for the package 
+or set the __all__ variable.
+'''
