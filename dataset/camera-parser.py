3ff0303d56a5956d917d35e4bae90f37dbeef61f
Liam Fogarty <lfogarty9995@gmail.com>
2023-05-17 13:46:27 +0200

    test camera parser on some calibration.json file

---
+++ dataset/camera-parser.py
@@ -0,0 +1,25 @@
+import json
+import io
+import numpy as np
+import torch
+
+def readCameraParameters(filename):
+    f = open(filename, "r")
+    data = json.load(f)
+    
+    K, R, t = [], [], []
+    
+    for camera in data['cameras']:
+        if camera['type'] == 'hd':
+            K.append(torch.from_numpy(np.array(camera['K'])))
+            R.append(torch.from_numpy(np.array(camera['R'])))
+            t.append(torch.from_numpy(np.array(camera['t'])))
+    
+    K = torch.stack(K, dim=0)
+    R = torch.stack(R, dim=0)
+    t = torch.stack(t, dim=0)
+    
+    return K, R, t
+
+
+K, R, t = readCameraParameters("dataset/calibration_170307_dance1.json")
