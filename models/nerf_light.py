ec1854ed45bebe2d79b8e50df9d8521bf27ee358
brianne <brianne.oberson@gmail.com>
2023-07-18 17:43:03 +0200

    Make funciton for rendering an image.

--- models/nerf_light.py
+++ models/nerf_light.py
@@ -2,7 +2,7 @@ import pytorch_lightning as pl
 import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from dataloader import NerfDataset
+from dataloaders.silhouette_dataloader import SilhouetteDataset
 from pytorch3d.renderer import (
     RayBundle,
     ray_bundle_to_ray_points,
@@ -266,17 +266,22 @@ class Nerf(pl.LightningModule):
                 eval_K = K[None, 0, ...]
                 eval_R = R[None, 0, ...]
                 eval_t = t[None, 0, ...]
-                full_silhouette, _ =  self.renderer_grid(
-                cameras=FoVPerspectiveCameras(K=eval_K, R=eval_R, T=eval_t, device=self.device),
-                volumetric_function=self.batched_forward
-                )
-                # _, full_silhouette = (full_silhouette.split([3,1], dim=-1))
-                clamp_and_detach = lambda x: x.clamp(0.0, 1.0)
-                silhouette_image = clamp_and_detach(full_silhouette[...,0])
-                self.logger.experiment.add_image('silhouette image', silhouette_image, global_step=self.current_epoch)
+                silhouette_image = self._render_image(camera=FoVPerspectiveCameras(K=eval_K, R=eval_R, T=eval_t, device=self.device))
+                self.logger.experiment.add_image('silhouette image', silhouette_image, global_step=self.current_epoch, dataformats='HWC' )
                 self.logger.experiment.add_histogram("histogram", silhouette_image, global_step=self.current_epoch, bins='auto')
 
         return loss
+
+    def _render_image(self, camera):
+        full_silhouette, _ =  self.renderer_grid(
+                cameras=camera,
+                volumetric_function=self.batched_forward
+                )
+        clamp_and_detach = lambda x: x.clamp(0.0, 1.0).cpu().detach().numpy()
+        silhouette_image = clamp_and_detach(full_silhouette[...,0])
+        # reshape numpy array so that channel is last (dataformats='HWC')
+        silhouette_image = silhouette_image.reshape(self.config.dataset.img_height, self.config.dataset.img_width, 1)
+        return silhouette_image
 
     def _get_densities(self, features):
         """
@@ -308,7 +313,7 @@ class Nerf(pl.LightningModule):
                 param_group['lr'] = new_lr
 
     def train_dataloader(self):
-        dataset = NerfDataset(self.config)
+        dataset = SilhouetteDataset(self.config)
         dataloader = DataLoader(dataset, batch_size=self.config.trainer.batch_size, shuffle=True)
         return dataloader
     
