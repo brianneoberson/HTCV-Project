ea8fc01d21ee46d32e84b06400091ca09629dd26
liamfogarty <lfogarty9995@gmail.com>
2023-08-26 21:52:23 +0200

    update calls.py

--- calls.py
+++ calls.py
@@ -3,11 +3,7 @@ import os
 this_file_dir = os.path.dirname(os.path.realpath(__file__))
 
 configs = [
-    f"{this_file_dir}/configs/cow_uniform_lambda_0.yaml",
-    f"{this_file_dir}/configs/cow_complex_stratified.yaml",
-    f"{this_file_dir}/configs/cow_stratified_lambda_0.5.yaml",
-    f"{this_file_dir}/configs/cow_stratified_lambda_0.yaml",
-    f"{this_file_dir}/configs/cow_color.yaml",
+    f"{this_file_dir}/configs/dance_sc_stratified.yaml",
 ]
 
 for config in configs:
